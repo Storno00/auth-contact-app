3ccdaba15c880b05c3da1f2650d2e9ae19845f13
Storno00 <benpeter00@gmail.com>
2023-05-30 14:25:11 +0200

    remove main css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import  { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Routes from './Routes.jsx';
 import { AuthProvider } from './contexts/AuthProvider';
 
