3795321dd5e2aa2dcda647f1e2b947a53254dec0
Storno00 <benpeter00@gmail.com>
2023-05-30 14:25:47 +0200

    constants

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,4 @@
+export const API_URL = import.meta.env.API_URL;
+export const AUTH_URL = `${API_URL}/auth`;
+export const CONTACT_URL = `${API_URL}/api/contact`;
+export const USER_URL = `${API_URL}/api/user`;
